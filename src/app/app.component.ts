00a4f050fdbc532d580d124c49ef39b748dbfdf4
Manu A T <ManuAT@users.noreply.github.com>
2020-02-27 00:45:21 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+declare var VANTA;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'web';
+  // VANTA : any;
+  ngOnInit(){
+    VANTA.BIRDS({
+      el: "#body1",
+      mouseControls: true,
+      touchControls: true,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      colorMode: "lerp"
+    })
+  }
+//  
+test(){
+  // console.log("hello");
+  
+  // VANTA.BIRDS({
+  //   el: "#body1",
+  //   mouseControls: true,
+  //   touchControls: true,
+  //   minHeight: 200.00,
+  //   minWidth: 200.00,
+  //   scale: 1.00,
+  //   scaleMobile: 1.00,
+  //   colorMode: "lerp"
+  // })
+
+}
+// window.onload = function () {
+//   VANTA.BIRDS({
+//     el: "#body1",
+//     mouseControls: true,
+//     touchControls: true,
+//     minHeight: 200.00,
+//     minWidth: 200.00,
+//     scale: 1.00,
+//     scaleMobile: 1.00,
+//     colorMode: "lerp"
+//   });
+// }
+
+
 }
